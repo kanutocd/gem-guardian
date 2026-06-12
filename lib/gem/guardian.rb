773c4cf264a680ff6dede6b0c9c8a8f620629ac4
Ken C. Demanawa <kenneth.c.demanawa@gmail.com>
2026-06-12 08:57:37 +0800

    Wire all them up

--- lib/gem/guardian.rb
+++ lib/gem/guardian.rb
@@ -1,10 +1,11 @@
 # frozen_string_literal: true
 
 require_relative "guardian/version"
-
-module Gem
-  module Guardian
-    class Error < StandardError; end
-    # Your code goes here...
-  end
-end
+require_relative "guardian/error"
+require_relative "guardian/checksum"
+require_relative "guardian/dependency"
+require_relative "guardian/lockfile_parser"
+require_relative "guardian/rubygems_client"
+require_relative "guardian/artifact_store"
+require_relative "guardian/verifier"
+require_relative "guardian/cli"
